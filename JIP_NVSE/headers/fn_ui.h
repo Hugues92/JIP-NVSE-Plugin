8527b872d1e82f839baa08a243cea0283a75614b
Jef <glenyavetz@yahoo.com>
2015-07-04 16:04:02 +0300

    Update fn_ui.h

--- JIP_NVSE/headers/fn_ui.h
+++ JIP_NVSE/headers/fn_ui.h
@@ -12,10 +12,11 @@ DEFINE_COMMAND_PLUGIN(GetActiveUIComponentID, "None", 0, 0, NULL);
 DEFINE_COMMAND_PLUGIN(GetMenuTargetRef, "None", 0, 1, kParams_OneInt);
 DEFINE_COMMAND_PLUGIN(GetMenuItemFilter, "None", 0, 2, kParams_OneInt_OneOptionalInt);
 DEFINE_COMMAND_PLUGIN(ClickMenuButton, "None", 0, 1, kParams_OneString);
+DEFINE_COMMAND_PLUGIN(GetSelectedItemRef, "None", 0, 0, NULL);
+DEFINE_COMMAND_PLUGIN(GetBarterItems, "None", 0, 1, kParams_OneInt);
+DEFINE_COMMAND_PLUGIN(GetBarterGoldAlt, "None", 0, 0, NULL);
 
-NiTArray<TileMenu*>* g_tileMenuArray = (NiTArray<TileMenu*>*)0x11F3508;
-
-Tile *GetTargetComponent(const char *componentPath, TileMenu *&parentMenu)
+Tile *GetTargetComponent(const char *componentPath, TileMenu **parentMenu = NULL)
 {
 	const char *slashPos = strpbrk(componentPath, "\\/");
 	std::string menuName("&");
@@ -26,10 +27,10 @@ Tile *GetTargetComponent(const char *componentPath, TileMenu *&parentMenu)
 	UInt32 menuType = Tile::TraitNameToID(menuName.data());
 	if ((menuType >= kMenuType_Min) && (menuType <= kMenuType_Max))
 	{
-		Tile *component = g_tileMenuArray->Get(menuType - kMenuType_Min);
+		Tile *component = g_TileMenuArray->Get(menuType - kMenuType_Min);
 		if (component)
 		{
-			parentMenu = (TileMenu*)component;
+			if (parentMenu) *parentMenu = (TileMenu*)component;
 			if (slashPos) component = component->GetComponentTile(slashPos + 1);
 			return component;
 		}
@@ -44,8 +45,7 @@ bool Cmd_IsComponentLoaded_Execute(COMMAND_ARGS)
 
 	if (ExtractArgs(EXTRACT_ARGS, &componentPath))
 	{
-		TileMenu *parentMenu;
-		if (InterfaceManager::GetMenuComponentValue(componentPath) || GetTargetComponent(componentPath, parentMenu)) *result = 1;
+		if (InterfaceManager::GetMenuComponentValue(componentPath) || GetTargetComponent(componentPath)) *result = 1;
 	}
 	if (IsConsoleMode()) Console_Print("Component Loaded >> %.0f", *result);
 	return true;
@@ -53,12 +53,7 @@ bool Cmd_IsComponentLoaded_Execute(COMMAND_ARGS)
 
 bool InjectXML(Tile *component, const char *xmlPath)
 {
-#if RUNTIME_VERSION == RUNTIME_VERSION_1_4_0_525
-	if (ThisStdCall(0xA01B00, component, xmlPath)) return true;
-#elif RUNTIME_VERSION == RUNTIME_VERSION_1_4_0_525ng
-	if (ThisStdCall(0xA019D0, component, xmlPath)) return true;
-#endif
-	return false;
+	return ThisStdCall(s_InjectXML, component, xmlPath) ? true : false;
 }
 
 bool Cmd_InjectUIXML_Execute(COMMAND_ARGS)
@@ -69,8 +64,7 @@ bool Cmd_InjectUIXML_Execute(COMMAND_ARGS)
 
 	if (ExtractArgs(EXTRACT_ARGS, &componentPath, &xmlPath))
 	{
-		TileMenu *parentMenu;
-		Tile *component = GetTargetComponent(componentPath, parentMenu);
+		Tile *component = GetTargetComponent(componentPath);
 		if (component) *result = InjectXML(component, xmlPath);
 	}
 	return true;
@@ -86,8 +80,7 @@ bool Cmd_InjectUIComponent_Execute(COMMAND_ARGS)
 
 	if (ExtractFormatStringArgs(1, tileString, paramInfo, scriptData, opcodeOffsetPtr, scriptObj, eventList, kCommandInfo_InjectUIComponent.numParams, &componentPath))
 	{
-		TileMenu *parentMenu;
-		Tile *component = GetTargetComponent(componentPath, parentMenu);
+		Tile *component = GetTargetComponent(componentPath);
 		if (component)
 		{
 			IFileStream tempFile;
@@ -102,13 +95,6 @@ bool Cmd_InjectUIComponent_Execute(COMMAND_ARGS)
 	}
 	return true;
 }
-
-typedef bool (*_IsMenuMode)(void);
-#if RUNTIME_VERSION == RUNTIME_VERSION_1_4_0_525
-const _IsMenuMode IsMenuMode = (_IsMenuMode)0x702360;
-#elif RUNTIME_VERSION == RUNTIME_VERSION_1_4_0_525ng
-const _IsMenuMode IsMenuMode = (_IsMenuMode)0x7022C0;
-#endif
 
 bool Cmd_GetCursorPos_Execute(COMMAND_ARGS)
 {
@@ -149,14 +135,14 @@ bool Cmd_GetUIString_Execute(COMMAND_ARGS)
 bool Cmd_GetActiveMenuMode_Execute(COMMAND_ARGS)
 {
 	*result = 0;
-	InterfaceManager *intfc = InterfaceManager::GetSingleton();
+	InterfaceManager *intfc = *g_interfaceManager;
 	if (intfc && intfc->activeMenu) *result = intfc->activeMenu->id;
 	return true;
 }
 
 bool Cmd_GetActiveUIComponentName_Execute(COMMAND_ARGS)
 {
-	InterfaceManager *intfc = InterfaceManager::GetSingleton();
+	InterfaceManager *intfc = *g_interfaceManager;
 	if (intfc && intfc->activeTile)
 	{
 		g_string->Assign(PASS_COMMAND_ARGS, intfc->activeTile->name.CStr());
@@ -168,7 +154,7 @@ bool Cmd_GetActiveUIComponentName_Execute(COMMAND_ARGS)
 
 bool Cmd_GetActiveUIComponentFullName_Execute(COMMAND_ARGS)
 {
-	InterfaceManager *intfc = InterfaceManager::GetSingleton();
+	InterfaceManager *intfc = *g_interfaceManager;
 	if (intfc && intfc->activeTile)
 	{
 		g_string->Assign(PASS_COMMAND_ARGS, intfc->activeTile->GetQualifiedName().data());
@@ -181,7 +167,7 @@ bool Cmd_GetActiveUIComponentFullName_Execute(COMMAND_ARGS)
 bool Cmd_GetActiveUIComponentID_Execute(COMMAND_ARGS)
 {
 	*result = -1;
-	InterfaceManager *intfc = InterfaceManager::GetSingleton();
+	InterfaceManager *intfc = *g_interfaceManager;
 	if (intfc && intfc->activeTile)
 	{
 		Tile::Value *val = intfc->activeTile->GetValue(4010);
@@ -195,24 +181,27 @@ bool Cmd_GetMenuTargetRef_Execute(COMMAND_ARGS)
 	UInt32 *refResult = (UInt32*)result, menuID = 0;
 	*refResult = 0;
 
-	if (IsMenuMode() && ExtractArgs(EXTRACT_ARGS, &menuID))
-	{
-		UInt32 refOffset;
-		if (menuID == 1008) refOffset = 29;
-		else if ((menuID == 1009) || (menuID == 1053)) refOffset = 32;
-		else if (menuID == 1014) refOffset = 27;
-		else if (menuID == 1055) refOffset = 102;
-		else if (menuID == 1057) refOffset = 43;
-		else if (menuID == 1075) refOffset = 26;
-		else return true;
-
-		TileMenu *tileMenu = g_tileMenuArray->Get(menuID - kMenuType_Min);
-		if (tileMenu && tileMenu->menu)
-		{
-			TESForm *menuRef = (TESForm*)*((UInt32*)tileMenu->menu + refOffset);
-			if (menuRef->TryGetREFRParent()) *refResult = menuRef->refID;
-		}
-	}
+	if (!IsMenuMode() || !ExtractArgs(EXTRACT_ARGS, &menuID)) return true;
+	TileMenu *tileMenu = g_TileMenuArray->Get(menuID - kMenuType_Min);
+	if (!tileMenu || !tileMenu->menu) return true;
+	TESObjectREFR *menuRef = NULL;
+	if (menuID == 1008)
+		menuRef = ((ContainerMenu*)tileMenu->menu)->containerRef;
+	else if (menuID == 1009)
+		menuRef = ((DialogMenu*)tileMenu->menu)->partnerRef;
+	else if (menuID == 1014)
+		menuRef = ((LockPickMenu*)tileMenu->menu)->targetRef;
+	else if (menuID == 1053)
+		menuRef = ((BarterMenu*)tileMenu->menu)->merchantRef;
+	else if (menuID == 1055)
+		menuRef = ((HackingMenu*)tileMenu->menu)->targetRef;
+	else if (menuID == 1056)
+		menuRef = ((VATSMenu*)tileMenu->menu)->targetRef;
+	else if (menuID == 1057)
+		menuRef = ((ComputersMenu*)tileMenu->menu)->targetRef;
+	else if (menuID == 1075)
+		menuRef = ((CompanionWheelMenu*)tileMenu->menu)->companionRef;
+	if (menuRef->TryGetREFRParent()) *refResult = menuRef->refID;
 	return true;
 }
 
@@ -221,17 +210,15 @@ bool Cmd_GetMenuItemFilter_Execute(COMMAND_ARGS)
 	*result = -1;
 	UInt32 menuID = 0, useRef = 0;
 
-	if (IsMenuMode() && ExtractArgs(EXTRACT_ARGS, &menuID, &useRef))
-	{
-		UInt32 valOffset;
-		if (menuID == 1002) valOffset = 33;
-		else if (menuID == 1008) valOffset = 35 + (useRef != 0);
-		else if (menuID == 1053) valOffset = 39 + (useRef != 0);
-		else return true;
-
-		TileMenu *tileMenu = g_tileMenuArray->Get(menuID - kMenuType_Min);
-		if (tileMenu && tileMenu->menu) *result = *((UInt32*)tileMenu->menu + valOffset) + (menuID == 1002);
-	}
+	if (!IsMenuMode() || !ExtractArgs(EXTRACT_ARGS, &menuID, &useRef)) return true;
+	TileMenu *tileMenu = g_TileMenuArray->Get(menuID - kMenuType_Min);
+	if (!tileMenu || !tileMenu->menu) return true;
+	if (menuID == 1002)
+		*result = ((InventoryMenu*)tileMenu->menu)->filter + 1;
+	else if (menuID == 1008)
+		*result = useRef ? ((ContainerMenu*)tileMenu->menu)->rightFilter : ((ContainerMenu*)tileMenu->menu)->leftFilter;
+	else if (menuID == 1053)
+		*result = useRef ? ((BarterMenu*)tileMenu->menu)->rightFilter : ((BarterMenu*)tileMenu->menu)->leftFilter;
 	return true;
 }
 
@@ -243,7 +230,7 @@ bool Cmd_ClickMenuButton_Execute(COMMAND_ARGS)
 	if (IsMenuMode() && ExtractArgs(EXTRACT_ARGS, &componentPath))
 	{
 		TileMenu *parentMenu;
-		Tile *component = GetTargetComponent(componentPath, parentMenu);
+		Tile *component = GetTargetComponent(componentPath, &parentMenu);
 		if (component)
 		{
 			Tile::Value *val = component->GetValue(4010);
@@ -253,6 +240,92 @@ bool Cmd_ClickMenuButton_Execute(COMMAND_ARGS)
 				*result = 1;
 			}
 		}
+	}
+	return true;
+}
+
+bool Cmd_GetSelectedItemRef_Execute(COMMAND_ARGS)
+{
+	UInt32 *refResult = (UInt32*)result;
+	*refResult = 0;
+
+	if (!IsMenuMode() || !s_nvseInvRefGetRef) return true;
+	InterfaceManager *intfc = *g_interfaceManager;
+	if (!intfc || !intfc->activeMenu) return true;
+	UInt32 menuID = intfc->activeMenu->id;
+	TESObjectREFR *target = NULL, *itemRef = NULL;
+
+	if (menuID == 1002)
+		itemRef = CreateRefForStack(*g_thePlayer, *g_inventoryMenuSelection);
+	else if (menuID == 1008)
+	{
+		ContainerMenu *conMenu = (ContainerMenu*)intfc->activeMenu;
+		if (conMenu->rightActive)
+		{
+			target = conMenu->containerRef;
+			if (!target) return true;
+		}
+		itemRef = CreateRefForStack(target ? target : *g_thePlayer, *g_containerMenuSelection);
+	}
+	else if (menuID == 1053)
+	{
+		BarterMenu *brtMenu = (BarterMenu*)intfc->activeMenu;
+		if (brtMenu->rightActive)
+		{
+			target = GetMerchantContainer(brtMenu->merchantRef);
+			if (!target) return true;
+		}
+		itemRef = CreateRefForStack(target ? target : *g_thePlayer, *g_barterMenuSelection);
+	}
+	if (itemRef) *refResult = itemRef->refID;
+	return true;
+}
+
+bool Cmd_GetBarterItems_Execute(COMMAND_ARGS)
+{
+	NVSEArrayVarInterface::Array *barterItems = g_array->CreateArray(NULL, 0, scriptObj);
+	g_array->AssignCommandResult(barterItems, result);
+
+	UInt32 sold = 0;
+	if (!IsMenuMode() || !s_nvseInvRefGetRef || !ExtractArgs(EXTRACT_ARGS, &sold)) return true;
+
+	TileMenu *tileMenu = g_TileMenuArray->Get(52);
+	if (!tileMenu || !tileMenu->menu) return true;
+
+	BarterMenu *brtMenu = (BarterMenu*)tileMenu->menu;
+
+	TESObjectREFR *target, *itemRef;
+	BarterItemList *barterList;
+	if (sold)
+	{
+		target = GetMerchantContainer(brtMenu->merchantRef);
+		if (!target) return true;
+		barterList = &brtMenu->rightBarter;
+	}
+	else
+	{
+		target = *g_thePlayer;
+		barterList = &brtMenu->leftBarter;
+	}
+	NVSEArrayVarInterface::Element *elem;
+	for (auto iter = barterList->Begin(); !iter.End(); ++iter)
+	{
+		if (itemRef = CreateRefForStack(target, iter.Get()))
+		{
+			elem = new NVSEArrayVarInterface::Element(itemRef);
+			g_array->AppendElement(barterItems, *elem);
+		}
+	}
+	return true;
+}
+
+bool Cmd_GetBarterGoldAlt_Execute(COMMAND_ARGS)
+{
+	*result = 0;
+	if (IsMenuMode())
+	{
+		TileMenu *tileMenu = g_TileMenuArray->Get(52);
+		if (tileMenu && tileMenu->menu) *result = ((BarterMenu*)tileMenu->menu)->merchantGold;
 	}
 	return true;
 }
